463ac3cf3fa86fe4a7e2af0a98f7dcd1330f2e8a
RONIT SINGH <ronitsingh7003@gmail.com>
2022-07-25 11:45:05 +0530

    #44 Creating Routes & Schema for Storing Notes & Users

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.send("Hello Ronit!");
 });
 
+// Available Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
---
+++ backend/routes/notes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  obj = {
+    a: "notes",
+    number: 34,
+  };
+  res.json(obj);
+});
+module.exports = router;
