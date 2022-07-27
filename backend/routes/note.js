95599b72cb5905013734dd026b40b60aa20ac474
RONIT SINGH <ronitsingh7003@gmail.com>
2022-07-27 13:23:33 +0530

    #53 Updating an existing Note

--- backend/routes/note.js
+++ backend/routes/note.js
@@ -45,4 +45,37 @@ router.post(
   }
 );
 
+router.put("/updatenote/:id", fetchuser, async function (req, res) {
+  try {
+    const { title, description, tag } = req.body;
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+    if (note.user.toString() != req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+    note = await Note.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    );
+    res.json(note);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 module.exports = router;
