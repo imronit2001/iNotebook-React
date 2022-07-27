7d34f0b105b88b0939405061e1b6bd62ab959c0a
RONIT SINGH <ronitsingh7003@gmail.com>
2022-07-27 18:21:04 +0530

    #58 useContext hook : Using React Context API

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import NoteState from "./context/notes/NoteState";
 
 const App = () => {
   return (
     <React.Fragment>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
-      </Routes>
+      <NoteState>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </NoteState>
     </React.Fragment>
   );
 };
--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useContext } from "react";
+import NoteContext from "../context/notes/NoteContext";
 
 const About = () => {
-  return <div>About</div>;
+  const a = useContext(NoteContext);
+  useEffect(() => {
+    a.update();
+    // eslint-disable-next-line
+  }, []);
+  return (
+    <React.Fragment>
+      <div>
+        My name is {a.state.name} and i am in {a.state.class}
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default About;
