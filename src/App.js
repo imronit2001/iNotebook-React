9b15808eb61db2988e713e30cd7868c7133510fe
RONIT SINGH <ronitsingh7003@gmail.com>
2022-07-27 17:56:24 +0530

    #56 Creating Navbar and Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <h1>Hello World</h1>;
-}
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+    </React.Fragment>
+  );
+};
 
 export default App;
